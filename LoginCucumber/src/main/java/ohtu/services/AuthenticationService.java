48b21fa9c1251440b41305952858ac8e6e669ca0
Eero Kuurne <eero.kuurne@helsinki.fi>
2017-04-02 17:41:29 +0300

    Exercise 5 Cucumber tests and AuthenticationService fix

--- LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
+++ LoginCucumber/src/main/java/ohtu/services/AuthenticationService.java
@@ -40,7 +40,34 @@ public class AuthenticationService {
 
     private boolean invalid(String username, String password) {
         // validity check of username and password
-
+        return usernameInvalid(username) || passwordInvalid(password);
+    }
+    
+    private boolean usernameInvalid(String username) {
+        if (username.length() < 3) {
+            return true;
+        }
+        for (int i = 0; i < username.length(); i++) {
+            char ch = username.charAt(i); 
+            if (!(ch >= 'a' && ch <= 'z') && !(ch >= 'A' && ch <= 'Z')) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean passwordInvalid(String password) {
+        if (password.length() < 8) {
+            return true;
+        }
+        for (int i = 0; i < password.length(); i++) {
+            if (!Character.isLetter(password.charAt(i))) {
+                break;
+            }
+            if (i == password.length() - 1) {
+                return true;
+            }
+        }
         return false;
     }
 }
